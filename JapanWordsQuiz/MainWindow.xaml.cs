7ec03a405f8345b6146c5408ca6a564c04100dcd
Какоткин Вячеслав <MetalSl8@yandex.ru>
2018-02-11 23:09:18 +0300

    organazed work

--- JapanWordsQuiz/MainWindow.xaml.cs
+++ JapanWordsQuiz/MainWindow.xaml.cs
@@ -4,23 +4,17 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using Microsoft.Win32;
+using System.Windows.Controls;
+using System.Diagnostics;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace JapanWordsQuiz
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-
-    /*internal class ListElem
-    {
-        public List<String[]> elems;
-        public ListElem()
-        {
-            elems = new List<string[]>();
-        }
-    }*/
+    [Serializable]
     internal class DictValue
     {
         public string[][] line;
@@ -31,62 +25,23 @@ namespace JapanWordsQuiz
     }
     public partial class MainWindow : Window
     {
-        //List<ListElem> dict_list = new List<ListElem>();
-        // if kana -> rus mode, then
-        int default_size = 3;
+        int default_size;
         Dictionary<int, DictValue> dict = new Dictionary<int, DictValue>();
         string[] true_answers;
-        const int SHIFT = 1;
-        int dict_len, num_of_ans, num_of_quest, curr_index;
-        //int[] key_indexes, value_indexes;
-        //string curr_hiragana, curr_kanzi;
+        int dict_len, num_of_ans, num_of_quest, curr_index, dict_num;
         public MainWindow()
         {
             InitializeComponent();
-            button2.IsEnabled = false;
-            num_of_quest = 0;
-            num_of_ans = 2;
+            radioButton3.IsEnabled = false;
+            back_button.IsEnabled = false;
+            OK_button.IsEnabled = false;
         }
     
-        private void button_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog fileDialog = new OpenFileDialog();
-            fileDialog.Filter = "Tabulated text files (*.tsv)|*.tsv"; // it's a lie
-            if (fileDialog.ShowDialog() != null)
-            {
-                try
-                {
-                    using (StreamReader sr = new StreamReader(fileDialog.FileName))
-                    {
-                        if (!Int32.TryParse(textBox.Text, out dict_len))
-                        {
-                            throw new ArgumentException("Количество строк должно быть числом.");
-                        }
-                        else if (dict_len == 0) throw new ArgumentException("Число не может быть равно нулю.");
-                        for (int i = 0; i < SHIFT; sr.ReadLine(), i++) ;
-                        for (int counter = SHIFT; counter <= dict_len; counter++)
-                        {
-                            AnalyseLine(sr.ReadLine(), out int key, out DictValue value);
-                            dict[key] = value;
-                        }
-                    }
-                }
-                catch (ArgumentException ex)
-                {
-                    MessageBox.Show("Возникла ошибка: " + ex.Message);
-                    return;
-                }
-                button2.IsEnabled = true;
-                textBlock1.Text = "";
-                textBlock2.Text = "";
-            }
-        }
-
         private void LetNewWord()
         {
             Random r = new Random();
             curr_index = r.Next(1, dict_len);
-            true_answers = (string[])dict[curr_index].line[num_of_ans].Clone();//(string[])dict[curr_index].line[num_of_ans].Clone();
+            true_answers = (string[])dict.Values.ElementAt(curr_index).line[num_of_ans].Clone();//(string[])dict[curr_index].line[num_of_ans].Clone();
             if (num_of_ans == 2)
             {
                 for (int i = 0; i < true_answers.Length; i++)
@@ -94,13 +49,39 @@ namespace JapanWordsQuiz
                     true_answers[i] = true_answers[i].ToUpper().Replace("ё", "е");
                 }
             }
-            textBlock1.Text = dict[curr_index].line[0][0]; //dict_list[curr_index].elems[0]/*column in table*/[0]/*num of word*/;
-            textBlock1.Text += " " + dict[curr_index].line[1][0];
+            question_text_block.Text = dict.Values.ElementAt(curr_index).line[0][0];
+            question_text_block.Text += " " + dict.Values.ElementAt(curr_index).line[1][0];
         }
 
-        private void button2_Click(object sender, RoutedEventArgs e)
+        private void button3_Click(object sender, RoutedEventArgs e)
         {
-            button2.IsEnabled = false;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            LetNewWord();
+            sw.Stop();
+            MessageBox.Show(sw.ElapsedMilliseconds.ToString());
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            StartButton.IsEnabled = false;
+            // deserialization
+            if (dict_num == 0)
+            {
+                MessageBox.Show("Выберите тип теста");
+                return;
+            }
+            if (!Int32.TryParse(dict_len_textblock.Text, out dict_len) & dict_len == 0)
+            {
+                MessageBox.Show("В поле 'Количество слов' введите число больше нуля");
+                return;
+            }
+            dict = Deserialize(dict_num);
+            StartButton.IsEnabled = false;
+            dict_len_textblock.IsEnabled = false;
+            OK_button.IsEnabled = true;
+            back_button.IsEnabled = true;
+            count_text_block.Text = dict_len.ToString();
             LetNewWord();
         }
 
@@ -115,28 +96,44 @@ namespace JapanWordsQuiz
                 string[] str_arr2 = str_arr[i].Split(new string[] { ", " }, StringSplitOptions.None);
                 value.line[i - 1] = str_arr2;
             }
-            /*try
-            {
-
-                var temp = from i in key_indexes select str_arr[i];
-                key = temp.ToArray();
-                temp = from i in value_indexes select str_arr[i];
-                value = temp.ToArray();
-                //foreach (var elem in key)
-                //{
-                //    MessageBox.Show(elem);
-                //}
-            }
-            catch (NullReferenceException)
-            {
-                throw;
-            }*/
         }
 
-        private async void button1_Click(object sender, RoutedEventArgs e)
+        private void radioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (((RadioButton)sender).Name == "radioButton")
+            {
+                dict_num = 1;
+                default_size = 3;
+                num_of_quest = 0;
+                num_of_ans = 2;
+            }
+            else if (((RadioButton)sender).Name == "radioButton1")
+            {
+                dict_num = 2;
+                default_size = 3;
+                num_of_quest = 1;
+                num_of_ans = 2;
+            }
+            else if (((RadioButton)sender).Name == "radioButton2")
+            {
+                dict_num = 3;
+                default_size = 3;
+                num_of_quest = 2;
+                num_of_ans = 0;
+            }
+            else if (((RadioButton)sender).Name == "radioButton3")
+            {
+                dict_num = 4;
+                default_size = 4;
+                num_of_quest = 0;
+                //num_of_ans = 1, 2;
+            }
+        }
+
+        private async void OK_button_Click(object sender, RoutedEventArgs e)
         {
             if (dict.Count == 0) return;
-            string user_answer = textBox1.Text;
+            string user_answer = answer_text_box.Text;
             if (user_answer == "")
             {
                 MessageBox.Show("Введите слово.");
@@ -146,22 +143,42 @@ namespace JapanWordsQuiz
                 user_answer = user_answer.ToUpper().Replace("ё", "е");
             if (true_answers.Contains(user_answer))
             {
-                textBlock2.Text = "Правильно!";
-                dict.Remove(curr_index);
+                status_text_block.Text = "Правильно!";
+                dict.Remove(dict.Keys.ElementAt(curr_index));
                 dict_len--;
                 textBox.Text = dict_len.ToString();
             }
-            else textBlock2.Text = "Ошибка.";
+            else status_text_block.Text = "Ошибка.";
             button1.IsEnabled = false;
-            textBox1.Text = "";
+            answer_text_box.Text = "";
             await Task.Run(() => Thread.Sleep(1000));
             button1.IsEnabled = true;
             if (dict_len > 0)
             {
-                textBlock2.Text = "...";
+                status_text_block.Text = "...";
                 LetNewWord();
             }
-            else textBlock2.Text = "Слова закончились.";
+            else status_text_block.Text = "Слова закончились.";
+        }
+
+        private void Serialize()
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream fs = new FileStream(@"C:\Users\user\source\repos\JapanWordsQuiz\dict_goi.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, dict);
+            }
+        }
+
+        private Dictionary<int, DictValue> Deserialize(int dict_num)
+        {
+            Dictionary<int, DictValue> new_dict;
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream fs = new FileStream(@"C:\Users\user\source\repos\JapanWordsQuiz\dict_goi.dat", FileMode.Open))
+            {
+                new_dict = (Dictionary<int, DictValue>)formatter.Deserialize(fs);
+            }
+            return new_dict;
         }
     }
 }
